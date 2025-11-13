31589f99b562f361797f910de80f38d9a29ee034
Abror305 <dilfuzaalmatova78@gmail.com>
2025-11-13 22:10:12 +0500

    ASDg

--- src/guard/PrivateRouter.jsx
+++ src/guard/PrivateRouter.jsx
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-
-const PrivateRouter = ({ children }) => {
-    // const user = useSelector(state => state.user?.isAuth)
-    const user = true
-    const navigate = useNavigate()
-
-
-    useEffect(() => {
-        console.log("user: ", user)
-        if (!user) {
-            navigate('/login')
-        }
-    }, [user, navigate])
-
-    return children
-}
-
-export default PrivateRouter
-=======
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
@@ -39,4 +15,3 @@ const PrivateRouter = ({ children }) => {
 };
 
 export default PrivateRouter;
->>>>>>> a203f86549066f90e149dcde0e5ea7e100221194
